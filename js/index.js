73b3a480dea988a8ac02b04c766c50aec087990d
Hildana Shewaneka <hildana.shi@gmail.com>
2023-05-14 04:44:57 +0300

    one before last

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-<script>
-    
-</script>
+const typewriter = document.getElementById("typewriter");
+const text = typewriter.innerText;
+let index = 0;
+
+typewriter.innerText = "";
+
+function type() {
+  if (index < text.length) {
+    typewriter.innerText += text.charAt(index);
+    index++;
+    setTimeout(type, 100);
+  }
+}
+
+type();
